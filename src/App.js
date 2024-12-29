0959ecd50f5b599646e29ae72aa55b42d9e5d1f6
Thanh Cong <phamthanhcong0709@gmail.com>
2024-12-29 11:14:41 +0700

    fix navigate when deploy Github Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from "./page/home/Home";
 import Products from "./page/products/Products";
 import AboutUs from "./page/about_us/AboutUs";
 import ContactUs from "./page/contact_us/ContactUs";
-import { Product } from "./page/product_detail/Product";
+import {Product} from "./page/product_detail/Product";
 import Checkout from "./page/checkout/Checkout";
 
 function App() {
     return (
         <div className="App">
-            <Router basename="/phe_la_coffee">
-                <Routes>
-                    <Route path='*' element={<Home/>}></Route>
-                    <Route path='/product' element={<Products/>}></Route>
-                    <Route path="/product/:id" element={<Product />} />
-                    <Route path='/about-us' element={<AboutUs/>}></Route>
-                    <Route path='/contact-us' element={<ContactUs/>}></Route>
-                    <Route path="/checkout" element={<Checkout />} />
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path='*' element={<Home/>}></Route>
+                <Route path='/product' element={<Products/>}></Route>
+                <Route path="/product/:id" element={<Product />} />
+                <Route path='/about-us' element={<AboutUs/>}></Route>
+                <Route path='/contact-us' element={<ContactUs/>}></Route>
+                <Route path="/checkout" element={<Checkout />} />
+            </Routes>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+    createHashRouter,
+    RouterProvider
+} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <App />,
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
